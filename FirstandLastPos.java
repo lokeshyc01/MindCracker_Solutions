58361acc990155e4cc03788fb7623a9d1f22a759
lokeshyc01 <tradingview8901@gmail.com>
2023-11-21 21:18:29 +0530

    added ans flag in recursive function call's

--- FirstandLastPos.java
+++ FirstandLastPos.java
@@ -10,9 +10,10 @@ public class FirstandLastPos {
     int start = 0;
     int end = arr.length;
     int mid = start + end / 2;
-    int first = findFirst(arr, start, mid, element);
+    int ans = -1;
+    int first = findFirst(arr, start, mid, element,ans);
     
-    int last = findLast(arr, mid + 1, end, element);
+    int last = findLast(arr, mid + 1, end, element,ans);
     if(first <= 0 && last >= 0)
     {
         System.out.println("First => "+last+" Last => "+last);
@@ -31,43 +32,47 @@ public class FirstandLastPos {
     }
   }
 
-  public static int findFirst(int arr[], int start, int end, int element) {
+  public static int findFirst(int arr[], int start, int end, int element,int ans) {
     // base conditions
+    
     System.out.println("in findfirst");
     printFlow(arr,start,end);
     if (start > end) {
-      return -1;
+      return ans;
     }
     int mid = start + end / 2;
     if (arr[mid] == element) {
-      return mid;
+      ans = mid;
+      end = mid - 1;
     } else if (arr[mid] > element) {
-      return findFirst(arr, start, mid - 1, element);
+      return findFirst(arr, start, end, element,ans);
     }
-    return findFirst(arr, mid + 1, end, element);
+    return findFirst(arr, mid + 1, end, element,ans);
     // recursive relation
 
   }
 
-  public static int findLast(int arr[], int start, int end, int element) {
+  public static int findLast(int arr[], int start, int end, int element,int ans) {
     System.out.println("in findlast");
+   
     printFlow(arr,start,end);
     if (start > end) {
-      return -1;
+      return ans;
     }
 
     int mid = (start + end) / 2;
 
     if (arr[mid] == element) {
-      return mid;
+      ans = mid;
+      start = mid + 1;
     } else if (arr[mid] > element) {
-      return findLast(arr, start, mid - 1, element);
+      return findLast(arr, start, mid - 1, element,ans);
     }
-    return findLast(arr, mid + 1, end, element);
+    return findLast(arr, start, end, element,ans);
   }
 
   public static void main(String args[]) {
     int arr[] = new int[] { 1, 2, 4, 5, 5, 6 };
-    firstAndLastPost(arr, 6);
+    firstAndLastPost(arr, 5);
   }
 }
