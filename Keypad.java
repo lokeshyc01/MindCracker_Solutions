bf4126e846decbf98f0ce34531c4d645eea20d66
lokeshyc01 <tradingview8901@gmail.com>
2023-11-24 21:40:24 +0530

    KeyPad leetcode

---
+++ Keypad.java
@@ -0,0 +1,78 @@
+class Keypad{
+    public static boolean check(String str)
+    {
+        boolean flag = true;
+        StringBuilder line1 = new StringBuilder("qwertyuiop");
+        StringBuilder line2 = new StringBuilder("asdfghjkl");
+        StringBuilder line3 = new StringBuilder("zxcvbnm");
+
+        if(str.length() == 0)
+        {
+            return true;
+        }
+
+        char arr[] = str.toLowerCase().toCharArray();
+
+        if(line1.indexOf(String.valueOf(arr[0])) != -1)
+        {
+            flag = true;
+            for(int iCnt = 1; iCnt < arr.length;iCnt++)
+            {
+                if(line1.indexOf(String.valueOf(arr[iCnt])) == -1)
+                {
+                    flag = false;
+                    break;
+
+                }
+            }
+        }
+
+
+        if(line2.indexOf(String.valueOf(arr[0])) != -1)
+        {
+            flag = true;
+            for(int iCnt = 1; iCnt < arr.length;iCnt++)
+            {
+                if(line2.indexOf(String.valueOf(arr[iCnt])) == -1)
+                {
+                    flag = false;
+                    break;
+
+                }
+            }
+        }
+        
+        if(line3.indexOf(String.valueOf(arr[0])) != -1)
+        {
+            flag = true;
+            for(int iCnt = 1; iCnt < arr.length;iCnt++)
+            {
+                if(line3.indexOf(String.valueOf(arr[iCnt])) == -1)
+                {
+                    flag = false;
+                    break;
+
+                }
+            }
+        }
+
+        if(flag)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    public String[] findWords(String[] words) {
+        List<String> list = new ArrayList<>();
+        for(String str : words)
+        {
+           if(check(str))
+           {
+               list.add(str);
+           }
+        }
+        return list.toArray(new String[0]);
+    }
+}
