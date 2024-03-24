c180929a96ea9fd4b717738377a1df26cb2c60d4
lokeshyc01 <tradingview8901@gmail.com>
2024-03-24 09:14:01 +0530

    check kth bit is set

--- BitManipulation/CheckKthBit.java
+++ BitManipulation/CheckKthBit.java
@@ -2,21 +2,64 @@
 
 public class CheckKthBit
 {   
+    // 1st solution
+    // public static boolean checkBit(int n,int k)
+    // {
+    //     int x = 1;
+    //     for(int i = 0 ; i < k - 1 ; i++)
+    //     {
+    //         x = x * 2;
+    //     }
+
+    //     if((n & x )!= 0)
+    //     {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // 2nd solution
+    // public static boolean checkBit(int n,int k)
+    // {
+    //     int x = 1;
+    //     for(int i = 0 ; i < k - 1 ; i++)
+    //     {
+    //         n =n/ 2;
+    //     }
+
+    //     if((n & x )!= 0)
+    //     {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // optimized solution
+
+    // public static boolean checkBit(int n,int k)
+    // {
+    //     int x = 1;
+    //     x = x << (k-1);
+
+    //     if((n & x )!= 0)
+    //     {
+    //         return true;
+    //     }
+    //     return false;
+    // }
 
     public static boolean checkBit(int n,int k)
     {
-        int x = 1;
-        for(int i = 0 ; i < k - 1 ; i++)
-        {
-            x = x * 2;
-        }
+        n = n >> (k-1);
 
-        if((n & x )!= 0)
+        if((n & 1 )!= 0)
         {
             return true;
         }
         return false;
     }
+
+
     public static void main(String arr[])
     {
         System.out.println(checkBit(7,2));
