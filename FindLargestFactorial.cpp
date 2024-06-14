998f38fe03b0ec3ebb2abd3e2585f24c47cc28b7
lokeshyc01 <tradingview8901@gmail.com>
2024-06-14 09:05:30 +0530

    find largest factorial gfg

---
+++ FindLargestFactorial.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    void multiply(vector<int> & arr,int multiplier,int &size)
+    {
+        int carry = 0;
+        
+        for(int i = 0 ; i < size ; i++){
+            
+            int res = multiplier*arr[i];
+            res = res + carry;
+            carry = res / 10;
+            
+            arr[i] = res % 10;
+            
+        }
+        
+        while(carry > 0){
+            carry = carry % 10;
+            arr[size] = carry;
+            size++;
+            carry= carry / 10;
+        }
+    }
+    vector<int> factorial(int N){
+        
+        vector<int> arr(10000,0);
+        int size = 1;
+        arr[0] = 1;
+        
+        for(int i = 2 ; i <= N ; i++){
+            multiply(arr,i,size);
+        }
+        
+        vector<int>result;
+        for(int i = size - 1 ; i >= 0 ; i--)
+        {
+            result.push_back(arr[i]);
+        }
+        return result;
+    }
+};
